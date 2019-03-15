e1195bed9cdd898431d574d81f945fcdec657cdb
Enrico Marocco <enrico.marocco@telecomitalia.it>
2019-03-15 11:19:32 +0100

    First import

---
+++ precache-manifest.10a20746d9f4297f766a8b09ea92c677.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c72529bf8d6718e161576e7070d78f5c",
+    "url": "/img/front-left.c72529bf.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b5af5b667bfa9984dd3824d6d41a92d2",
+    "url": "/img/circle.b5af5b66.svg"
+  },
+  {
+    "revision": "623ef3dbc292c27ec39b",
+    "url": "/css/chunk-vendors.7863b0ce.css"
+  },
+  {
+    "revision": "f7c686a16f33863c31916ebe55d58940",
+    "url": "/img/back-right.f7c686a1.svg"
+  },
+  {
+    "revision": "f8172123cf94face79e0acfaf32b2823",
+    "url": "/img/front-right.f8172123.svg"
+  },
+  {
+    "revision": "c5e773ffe0afbadc952e",
+    "url": "/js/app.d8635597.js"
+  },
+  {
+    "revision": "623ef3dbc292c27ec39b",
+    "url": "/js/chunk-vendors.23480bea.js"
+  },
+  {
+    "revision": "41aa44d1d75c9db175e3194cf513f756",
+    "url": "/img/back-left.41aa44d1.svg"
+  },
+  {
+    "revision": "1229a71cc37d351c3627d5bf9b3d39c1",
+    "url": "/img/left.1229a71c.svg"
+  },
+  {
+    "revision": "e5420147d30bf5fd8cc965add150b619",
+    "url": "/img/right.e5420147.svg"
+  },
+  {
+    "revision": "08e60d3d1e488bc2b43da80ed7104f6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5e773ffe0afbadc952e",
+    "url": "/css/app.f09fc51c.css"
+  }
+];
